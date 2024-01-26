9a91a95cf8eb6c732901d0073265ae9bf95c3847
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-01-26 13:42:46 +0200

    advanced results middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,6 +11,8 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -27,7 +29,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
 // Specify routes for methods
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 
--- routes/courses.js
+++ routes/courses.js
@@ -7,14 +7,17 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
-// const course = require('../models/Course');
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
-// const advancedResults = require('../middleware/advancedResults');
 // const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(getCourses).post(addCourse);
+router.route('/').get(advancedResults(Course, {
+  path: 'bootcamp',
+  select: 'name description',
+}), getCourses).post(addCourse);
 
 router.route('/:id')
   .get(getCourse)
