2a8ae10e66e645267499815cf159bb913147d72b
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-01-30 17:53:45 +0200

    user ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,7 +31,24 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to request, body
+  req.body.user = req.user.id; // Now req body has the user on it
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add 1 bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with id: ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
+
   res.status(201).json({
     success: true,
     data: bootcamp,
@@ -42,15 +59,29 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+  let bootcamp = await Bootcamp.findById(req.params.id, req.body, {
     new: true,
     runValidators: true,
-  });
+  }).populate('user');
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id: ${req.params.id}`, 404)
     );
   }
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({ success: true, data: bootcamp });
 });
 
@@ -65,7 +96,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   const deletedBootcamp = await bootcamp.deleteOne();
-  await Course.deleteMany({ bootcamp: deletedBootcamp.id});
+  await Course.deleteMany({ bootcamp: deletedBootcamp.id });
 
   res.status(200).json({ success: true, data: {} });
 });
@@ -102,38 +133,39 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id/photo
 // @access  Private
 exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
-  
   const bootcamp = await Bootcamp.findById(req.params.id);
   const file = req.files.file;
-  const MAXSIZE= process.env.MAX_FILE_UPLOAD;
+  const MAXSIZE = process.env.MAX_FILE_UPLOAD;
   const UPLOADPATH = process.env.FILE_UPLOAD_PATH;
-  
+
   if (!bootcamp) {
-    return next(new ErrorResponse(`Bootcamp not found with id: ${req.params.id}`, 404));
+    return next(
+      new ErrorResponse(`Bootcamp not found with id: ${req.params.id}`, 404)
+    );
   }
-  if(!req.files){
+  if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
-  if(!file.mimetype.startsWith('image')){
+  if (!file.mimetype.startsWith('image')) {
     return next(new ErrorResponse(`Please upload an image file`, 400));
   }
-  if(file.size> MAXSIZE){
-    const sizeInMb = MAXSIZE/1000000;
-    return next(new ErrorResponse(`Please upload an image less than ${sizeInMb}MB`, 400));
+  if (file.size > MAXSIZE) {
+    const sizeInMb = MAXSIZE / 1000000;
+    return next(
+      new ErrorResponse(`Please upload an image less than ${sizeInMb}MB`, 400)
+    );
   }
 
   // You can use timestamp or any other format for naming
   file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
   // Move file
-  file.mv(`${UPLOADPATH}/${file.name}`, async err =>{
-    if(err){
+  file.mv(`${UPLOADPATH}/${file.name}`, async (err) => {
+    if (err) {
       console.log(err);
       return next(new ErrorResponse(`Problem with file upload`, 500));
     }
-    await Bootcamp.findByIdAndUpdate(req.params.id, {photo: file.name});
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
 
-    res.status(200).json({ success: true, data: file.name});
+    res.status(200).json({ success: true, data: file.name });
   });
-
-  
 });
